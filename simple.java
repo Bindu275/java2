42e0b62e055d5a7239f398ca90643eff0399840a
Iramtegbrahana123 <sayednsrddn@gmail.com>
2025-04-08 10:01:54 +0530

    saved

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
 public class simple {
     public static void main(String[] args) {
-        System.out.println("A Simple Java Program ");
+        System.out.println("A Simple Java Program");
     }
 }
